516cf3c9f442f19df1142c776167a8b07c2274dd
RahulSinghAswal <aswalrahul576@gmail.com>
2021-10-04 15:10:25 +0530

    From Mobile after linked to github from lapto

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div style={{background: "black"}}>
+            This is an home page
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum corporis odio, nesciunt sed eveniet, nemo amet, dignissimos magnam itaque ipsam possimus. Inventore vitae, perferendis harum quas cupiditate adipisci repellendus tempore asperiores at consequatur quos?</p>
+            <img src="https://www.pngkey.com/png/full/332-3321462_mobile-menu-for-barefoot-resort-vacations-hamburger-menu.png"  width="10%" />
+        </div>
+    )
+}
